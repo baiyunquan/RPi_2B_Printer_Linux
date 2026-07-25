3769cc38a51dc31a73237e693a0a71115d5e2787
baiyunquan <liaic@outlook.com>
2026-07-25 16:29:41 +0800

    Set persistent CUPS directory ownership

--- image/input/stages/60/20-install-print-stack.sh
+++ image/input/stages/60/20-install-print-stack.sh
@@ -18,5 +18,12 @@ fi
 
 install -d "$ROOTFS_PATH/var/spool/cups" "$ROOTFS_PATH/var/cache/cups"
 install -d "$DATAFS_PATH/var/spool/cups" "$DATAFS_PATH/var/cache/cups"
-chroot_exec chown root:lp /data/var/spool/cups /data/var/cache/cups
-chmod 710 "$DATAFS_PATH/var/spool/cups"
+lp_gid=$(awk -F: '$1 == "lp" { print $3 }' "$ROOTFS_PATH/etc/group")
+[ -n "$lp_gid" ] || {
+	echo "lp group is missing from the target root filesystem" >&2
+	exit 1
+}
+chown "0:$lp_gid" \
+	"$ROOTFS_PATH/var/spool/cups" "$ROOTFS_PATH/var/cache/cups" \
+	"$DATAFS_PATH/var/spool/cups" "$DATAFS_PATH/var/cache/cups"
+chmod 710 "$ROOTFS_PATH/var/spool/cups" "$DATAFS_PATH/var/spool/cups"
